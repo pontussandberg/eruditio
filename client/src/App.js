388aa2a3bdde63691406056294c1a182e4f2df56
Artur J Zapater <savagepixie@protonmail.com>
2020-07-20 18:35:50 +0200

    fixed linter configuration

--- client/src/App.js
+++ client/src/App.js
@@ -1,15 +1,15 @@
 import React from 'react';
-import GoogleOAuth from './components/GoogleOAuth.jsx'
+import GoogleOAuth from './components/GoogleOAuth.jsx';
 
 const App = () => {
     return (
         <>
             <GoogleOAuth />
             <div>
-            HELLO WORLD 
+            HELLO WORLD
             </div>
         </>
     );
-}
+};
 
 export default App;
