4d00fd917cd1e2827a37b323c5605da897d20aaa
Kris <kris.mutalapova@gmail.com>
2020-07-28 12:02:05 +0200

    the tutor can start the lesson

--- server/src/app.js
+++ server/src/app.js
@@ -4,6 +4,8 @@ const bodyParser = require('body-parser');
 const cookieSession = require('cookie-session');
 const passport = require('./lib/passport');
 const { auth, users } = require('./routes');
+const db = require('./lib/db');
+const uuid = require('uuid');
 
 const app = express();
 
@@ -22,5 +24,20 @@ app.use('/auth', auth);
 app.use('/api/users', users);
 
 app.get('/', (req, res) => res.redirect('http://localhost:3000'));
+
+app.post('/api/rooms', (req, res) => {
+    const id = uuid.v1();
+    db.createRoom({ id, ...req.body, tutor: req.user.shortId })
+        .then(() => res.json(id));
+});
+
+// app.route('/api/rooms/:id')
+//     .get((req, res) => {
+//         db.findRoom(req.params.id)
+//             .then(data => res.json(data));
+//     })
+//     .delete((req, res) => {
+//         db.deleteRoom()
+//     });
 
 module.exports = app;
--- server/src/lib/db.js
+++ server/src/lib/db.js
@@ -122,6 +122,27 @@ const declineRequest = (tutor, student) => {
         .finally(() => client.close());
 };
 
+const createRoom = room => {
+    const client = new MongoClient(mongoUri, mongoOpts);
+    return connect(client, 'rooms')
+        .then(col => col.insertOne(room))
+        .finally(() => client.close());
+};
+
+const deleteRoom = id => {
+    const client = new MongoClient(mongoUri, mongoOpts);
+    return connect(client, 'rooms')
+        .then(col => col.deleteOne({ id }))
+        .finally(() => client.close());
+};
+
+const findRoom = id => {
+    const client = new MongoClient(mongoUri, mongoOpts);
+    return connect(client, 'rooms')
+        .then(col => col.findOne({ id }))
+        .finally(() => client.close());
+};
+
 module.exports = {
     addRequest,
     createUser,
@@ -132,4 +153,7 @@ module.exports = {
     getPending,
     acceptRequest,
     declineRequest,
+    createRoom,
+    deleteRoom,
+    findRoom,
 };
