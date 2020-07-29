0b9b92643c4043ba5ba6b5990c1e32895ef21e69
Artur J Zapater <savagepixie@protonmail.com>
2020-07-29 13:18:27 +0200

    added Google icon

--- client/src/components/buttons/GoogleOAuth.jsx
+++ client/src/components/buttons/GoogleOAuth.jsx
@@ -1,6 +1,6 @@
 import React from 'react';
 
-const GoogleOAuth = ({classes}) => (
+const GoogleOAuth = ({ classes }) => (
     <a className={`${classes}`} href='http://localhost:5000/auth/google'>Google Login</a>
 );
 
