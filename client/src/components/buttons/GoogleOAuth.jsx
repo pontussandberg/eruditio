34251f17dc13917361ce7d5c4c01944288df8f5b
Artur J Zapater <savagepixie@protonmail.com>
2020-07-29 13:37:58 +0200

    added Google button

--- client/src/components/buttons/GoogleOAuth.jsx
+++ client/src/components/buttons/GoogleOAuth.jsx
@@ -1,7 +1,15 @@
 import React from 'react';
+import googleSVG from '../../media/google.svg';
 
 const GoogleOAuth = ({ classes }) => (
-    <a className={`${classes}`} href='http://localhost:5000/auth/google'>Google Login</a>
+    <a
+        style={{ backgroundColor: '#4285F4', borderColor: '#4285F4' }}
+        className={`flex border rounded-sm ${classes}`}
+        href='http://localhost:5000/auth/google'
+    >
+        <img style={{ width: '36px' }} className='bg-white p-2 rounded-sm' src={googleSVG} />
+        <span className='text-sm text-white font-semibold p-2'>Sign in with Google</span>
+    </a>
 );
 
 export default GoogleOAuth;
